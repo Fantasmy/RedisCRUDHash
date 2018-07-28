d2ceb9389378cf174fc0de2294be03928b400c2a
Jenny <fantazmit@gmail.com>
2018-07-28 22:05:47 +0200

    Windows form ok. Close #2.

--- RedisCRUDHash/Form1.Designer.cs
+++ RedisCRUDHash/Form1.Designer.cs
@@ -28,13 +28,196 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.boxId = new System.Windows.Forms.TextBox();
+            this.boxName = new System.Windows.Forms.TextBox();
+            this.boxSurname = new System.Windows.Forms.TextBox();
+            this.boxDni = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.StudentGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.StudentGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Surname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Id";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(61, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "DNI";
+            // 
+            // boxId
+            // 
+            this.boxId.Location = new System.Drawing.Point(146, 42);
+            this.boxId.Name = "boxId";
+            this.boxId.Size = new System.Drawing.Size(198, 26);
+            this.boxId.TabIndex = 4;
+            // 
+            // boxName
+            // 
+            this.boxName.Location = new System.Drawing.Point(146, 82);
+            this.boxName.Name = "boxName";
+            this.boxName.Size = new System.Drawing.Size(198, 26);
+            this.boxName.TabIndex = 5;
+            // 
+            // boxSurname
+            // 
+            this.boxSurname.Location = new System.Drawing.Point(146, 124);
+            this.boxSurname.Name = "boxSurname";
+            this.boxSurname.Size = new System.Drawing.Size(198, 26);
+            this.boxSurname.TabIndex = 6;
+            // 
+            // boxDni
+            // 
+            this.boxDni.Location = new System.Drawing.Point(146, 162);
+            this.boxDni.Name = "boxDni";
+            this.boxDni.Size = new System.Drawing.Size(198, 26);
+            this.boxDni.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(387, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 32);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(502, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 32);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(387, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 32);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(502, 130);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 32);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Cancel";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(387, 162);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 32);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Delete";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(626, 82);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(118, 71);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Refresh";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // StudentGridView
+            // 
+            this.StudentGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.StudentGridView.Location = new System.Drawing.Point(54, 216);
+            this.StudentGridView.Name = "StudentGridView";
+            this.StudentGridView.RowTemplate.Height = 28;
+            this.StudentGridView.Size = new System.Drawing.Size(690, 202);
+            this.StudentGridView.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StudentGridView);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.boxDni);
+            this.Controls.Add(this.boxSurname);
+            this.Controls.Add(this.boxName);
+            this.Controls.Add(this.boxId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.StudentGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox boxId;
+        private System.Windows.Forms.TextBox boxName;
+        private System.Windows.Forms.TextBox boxSurname;
+        private System.Windows.Forms.TextBox boxDni;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.DataGridView StudentGridView;
     }
 }
 
